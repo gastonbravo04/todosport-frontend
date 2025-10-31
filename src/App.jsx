329fb366d25653e7dc3f935b051869e007c88f05
gastonbravo04 <mg.bravo@alumno.um.edu.ar>
2025-10-31 01:00:40 -0300

    Añadir componente Favorites para mostrar productos marcados como favoritos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
 import React from 'react';
-import { Route, Routes, PrivateRoute } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import PrivateRoute from './routes/PrivateRoute';
 import Profile from './components/Profile';
-import Home from './components/Home';
+import Home from './views/Home';
 import StaffRoute from './routes/StaffRoute';
+import AdminPanel from './views/AdminPanel';
+import ProductDetail from './views/ProductDetail';
+import Cart from './views/Cart';
+import Checkout from './views/Checkout';
 
 const App = () => {
-return (
+  return (
     <Routes>
-    {/* públicas */}
-    <Route path="/login" element={<Login />} />
-    <Route path="/register" element={<Register />} />
-    <Route path="/" element={<Home />} />
+      {/* Rutas Públicas */}
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:productId" element={<ProductDetail />} />
 
-    {/* solo administradores */}
-    <Route element={<StaffRoute />}>
-        <Route path="/admin" element={<AdminDashboard />} />
-        {/* rutas para gestionar productos, etc. */}
-    </Route>
+      {/* Rutas Privadas (solo para usuarios autenticados) */}
+      <Route element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Route>
+
+      {/* Rutas de Staff (solo para administradores) */}
+      <Route element={<StaffRoute />}>
+        <Route path="/admin" element={<AdminPanel />} />
+      </Route>
     </Routes>
-);
+  );
 };
 
 export default App;
